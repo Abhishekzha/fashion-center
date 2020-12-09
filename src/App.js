7580ccbcc71b8eeff85c829eaad3de41af9f3653
Abhishek jha <zhaavisek@gmail.com>
2020-12-09 17:11:12 +0545

    added shop page and styles the collection items

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
+import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop-page/shop-page.component';
+import {Switch,Route} from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <HomePage/>
-    </div>
-  );
-}
-
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state={
+      currentUser:null
+    }
+   }
+    render(){
+       return(
+         <div>
+         <Switch>
+                <Route exact path='/' component={HomePage}/>
+                <Route exact path='/shop' component={ShopPage}/>
+         </Switch>
+         </div>
+       
+       )
+    }
+  }
+  
 export default App;
